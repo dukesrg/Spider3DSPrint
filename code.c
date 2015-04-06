9a2183f2b829e2a563ab4b9fac902703c0868519
dukesrg <dukesrg@gmail.com>
2015-04-07 00:40:20 +0300

    Update code.c

--- code.c
+++ code.c
@@ -3,6 +3,9 @@
 
 int uvl_entry()
 {
-	print("\vHello, \r\n\t");
-	print(0xDEADBEEF, 0x00FF7F00, COLOR_TRANS);
+	print("\fHello World!", 0x00000000, 0x00555555);
+	print("\vHello World!", 0x00000000, COLOR_TRANS);
+	print("\nHello World2", 0x000000FF, COLOR_TRANS);
+	print("\nHello World3", 0x000000FF, COLOR_TRANS);
+	printint(0xDEADBEEF, 0x0000FFFF, COLOR_TRANS);
 }
